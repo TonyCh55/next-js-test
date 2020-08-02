35f1aa05e8c7cb94da02326610f9c44c670de1c7
TonyCh55 <chuprunant@gmail.com>
2020-08-02 13:19:06 +0300

    feat: test-app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,9 +15,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="stylesheet" href="nprogress.css" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
